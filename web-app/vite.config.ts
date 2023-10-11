724143712851c5e7f20bb3fdfe54fbf4e1ace79c
Giáp Huy <20021366@vnu.edu.vn>
2023-10-11 23:11:16 +0700

    setup vite alias

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+      'assets': "/src/assets",
+      'styles': '/src/styles',
+      'components': '/src/components',
+      'contexts': '/src/components',
+      'utils': '/src/components'
+    }
+  },
   plugins: [react()],
   build: {
     outDir: "../server/web-app"
